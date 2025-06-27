16e05d29d9e9275f568b86f9b53ed309203daf8b
mikle <mikle.kontinent@gmail.com>
2025-06-27 13:09:11 +0700

    Initial Commit

---
+++ Car Selection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car Selection
+//
+//  Created by Таня Кожевникова on 27.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
